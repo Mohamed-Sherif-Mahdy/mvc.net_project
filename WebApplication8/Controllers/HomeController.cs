d9cf5bf580c77c16bff1708c7c832431beb30d57
mohammedsherif <cds.Mohamedsherif21720@alexu.edu.eg>
2024-03-19 09:17:01 +0200

    Add project files.

---
+++ WebApplication8/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Properties.Controllers
+{
+  public class HomeController : Controller
+  {
+    DbConnection db = new DbConnection();
+    public IActionResult Index()
+    {
+      return View();
+    }
+    //home/contact?name=dd&phone=dsfd&Email=sdf%40gmail.com&message=sdfsdf
+    public IActionResult Contact(Contact data)
+    {
+      db.Contacts.Add(data);
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public IActionResult about()
+    {
+      return View();
+    }
+
+    public IActionResult service()
+    {
+      return View();
+    }
+    //home/contact
+    public IActionResult ContactPage()
+    {
+      return View();
+    }
+  }
+}
---
+++ WebApplication8/Models/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication8.Models
+{
+  public class DbConnection:DbContext
+  {
+   
+    public DbSet<Contact> Contacts { get; set; }
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer("" +
+        "Data Source=DESKTOP-S9OMEN7\\SQLEXPRESS;" +
+        "Initial Catalog=Contact;" +
+        "Integrated Security=True;" +
+        "TrustServerCertificate=True");
+    }
+
+  }
+}
